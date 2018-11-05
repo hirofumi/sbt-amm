70e8e127cde451c9184cc781d9f524598dda9920
Hirofumi Yoshikawa <hirofummy@gmail.com>
2018-11-05 21:37:59 +0900

    Update default Ammonite version (1.1.2 -> 1.4.0)

--- src/main/scala/com/github/hirofumi/sbt/AmmPlugin.scala
+++ src/main/scala/com/github/hirofumi/sbt/AmmPlugin.scala
@@ -36,7 +36,7 @@ object AmmPlugin extends AutoPlugin {
       ammPredefCode           := None,
       ammSilent               := false,
       ammSourceCommandSupport := false,
-      ammVersion              := (if (scalaBinaryVersion.value == "2.10") "1.0.3" else "1.1.2")
+      ammVersion              := (if (scalaBinaryVersion.value == "2.10") "1.0.3" else "1.4.0")
     ) ++ ammSettings(Compile, Amm) ++ ammSettings(Test, AmmTest)
 
   override def trigger: PluginTrigger =
